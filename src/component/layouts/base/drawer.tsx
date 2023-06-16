688d2195442c735aefacf06f25b624bc64ef091b
ridhoadtyaa <r2603an@gmail.com>
2023-06-16 11:19:47 +0700

    finishing deploy

--- src/component/layouts/base/drawer.tsx
+++ src/component/layouts/base/drawer.tsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss'
-import { listDrawer } from '../../../utils/listDrawer'
+import { SubDrawer, listDrawer } from '../../../utils/listDrawer'
 import { useState, useEffect } from 'react'
 import clsx from 'clsx'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
@@ -44,36 +44,42 @@ function Drawer() {
 		router.push('/')
 	}
 
+	const renderDrawer = (item: SubDrawer, index: number) => {
+		return (
+			<div key={index}>
+				<div className={styles.drawerItemMain} id={item.title} onClick={_handleClick}>
+					{item.title}
+					<KeyboardArrowDownIcon id={item.title} className={clsx(styles.drawerItemMainIcon, active.includes(item.title) && styles.active)} />
+				</div>
+				{item.sub.map((subItem, subIndex): any => {
+					return (
+						<div
+							key={subIndex}
+							className={clsx(styles.drawerItemSubItem, active.includes(item.title) && styles.active, path === subItem.link && styles.actived)}
+						>
+							<Link href={subItem.link} className="flex">
+								<div className={styles.drawerItemSubItemIcon}>{subItem.icon}</div>
+								<div className={styles.drawerItemSubItemName}>{subItem.name}</div>
+							</Link>
+						</div>
+					)
+				})}
+			</div>
+		)
+	}
+
 	return (
 		<div className="flex flex-col justify-between space-y-10">
 			<div className={styles.drawerWrapper}>
 				{_handleModalExpired()}
 				{listDrawer.map((item, index) => {
-					return (
-						<div key={index}>
-							<div className={styles.drawerItemMain} id={item.title} onClick={_handleClick}>
-								{item.title}
-								<KeyboardArrowDownIcon id={item.title} className={clsx(styles.drawerItemMainIcon, active.includes(item.title) && styles.active)} />
-							</div>
-							{item.sub.map((subItem, subIndex): any => {
-								return (
-									<div
-										key={subIndex}
-										className={clsx(styles.drawerItemSubItem, active.includes(item.title) && styles.active, path === subItem.link && styles.actived)}
-									>
-										<Link href={subItem.link} className="flex">
-											<div className={styles.drawerItemSubItemIcon}>{subItem.icon}</div>
-											<div className={styles.drawerItemSubItemName}>{subItem.name}</div>
-										</Link>
-									</div>
-								)
-							})}
-						</div>
-					)
+					return item.role === 'not-admin' && user?.data?.roleId !== 1
+						? renderDrawer(item, index)
+						: user?.data?.roleId === 1 && renderDrawer(item, index)
 				})}
 			</div>
 
-			<div className="ml-8">
+			<div className="ml-8 pb-6">
 				<button onClick={logoutHandler} className="text-lg flex items-center space-x-2">
 					<FiLogOut className="mr-2" />
 					Logout
--- src/component/layouts/base/index.tsx
+++ src/component/layouts/base/index.tsx
@@ -1,31 +1,41 @@
-import Button from "../../elements/Button";
-import Search from "../../elements/Search";
-import styles from "./styles.module.scss";
-import Drawer from "./drawer";
-import Logo from "@/assets/icons/logo.svg";
+import Button from '../../elements/Button'
+import Search from '../../elements/Search'
+import styles from './styles.module.scss'
+import Drawer from './drawer'
+import Logo from '@/assets/icons/logo.svg'
 import Image from 'next/image'
-export default function Base({children}:any) {
-  return (
-    <div className={styles.root}>
-      <div className={styles.sideBar}>
-        <div className={styles.title}>
-          <Image src={Logo} alt="logo"/>
-          <Drawer/>
-        </div>
-        
-      </div>
-      <div className={styles.navbarWrapper}>
-        <div className={styles.appBar}>
-          <h1>Gaboot Deployment Platform</h1>
-          <div className={styles.right}>
-            <Search placeholder="Cari"/>
-            <Button>Sync</Button>
-          </div>
-        </div>
-        <div className={styles.content}>
-          {children}
-        </div>
-      </div>
-    </div>
-  )
+import { UseAppSelector } from '@/redux/hooks'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+export default function Base({ children }: any) {
+	const user = UseAppSelector((state: any) => state.authLogin.user)
+	const router = useRouter()
+	useEffect(() => {
+		const pathone = router.pathname.split('/')
+		if (pathone[1] === 'administrator' && user.data.roleId !== 1) {
+			router.push('/main/dashboard')
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [router.pathname, user?.data?.roleId])
+
+	return (
+		<div className={styles.root}>
+			<div className={styles.sideBar}>
+				<div className={styles.title}>
+					<Image src={Logo} alt="logo" />
+					<Drawer />
+				</div>
+			</div>
+			<div className={styles.navbarWrapper}>
+				<div className={styles.appBar}>
+					<h1>Gaboot Deployment Platform</h1>
+					<div className={styles.right}>
+						<Search placeholder="Cari" />
+						<Button>Sync</Button>
+					</div>
+				</div>
+				<div className={styles.content}>{children}</div>
+			</div>
+		</div>
+	)
 }
