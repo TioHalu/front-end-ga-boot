62d4ee22e4e1d3ada91d1f773647a724da0c18b8
ridhoadtyaa <r2603an@gmail.com>
2023-06-17 22:57:49 +0700

    kube service dan image

--- src/utils/listDrawer.tsx
+++ src/utils/listDrawer.tsx
@@ -8,76 +8,76 @@ import AcUnitIcon from '@mui/icons-material/AcUnit'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest'
 export interface SubDrawer {
-	title: string
-	role?: string
-	sub: {
-		name: string
-		link: string
-		icon: any
-	}[]
+  title: string
+  role?: string
+  sub: {
+    name: string
+    link: string
+    icon: any
+  }[]
 }
 
 export const listDrawer: SubDrawer[] = [
-	{
-		title: 'Main',
-		role: 'not-admin',
-		sub: [
-			{
-				name: 'Dashboard',
-				link: '/main/dashboard',
-				icon: <HomeIcon />,
-			},
-			{
-				name: 'Deploy',
-				link: '/main/deploy',
-				icon: <CloudIcon />,
-			},
-			{
-				name: 'Image',
-				link: '/main/image',
-				icon: <StorageIcon />,
-			},
-			{
-				name: 'Services',
-				link: '/main/services',
-				icon: <MarkunreadMailboxIcon />,
-			},
-			{
-				name: 'Addons',
-				link: '/main/addons',
-				icon: <AddBoxIcon />,
-			},
-		],
-	},
-	{
-		title: 'Administrator',
-		sub: [
-			{
-				name: 'Member',
-				link: '/administrator/member',
-				icon: <PeopleAltIcon />,
-			},
-			{
-				name: 'Kube Config',
-				link: '/administrator/kube-config',
-				icon: <AcUnitIcon />,
-			},
-			{
-				name: 'Gitlab Config',
-				link: '/administrator/gitlab-config',
-				icon: <GitHubIcon />,
-			},
-		],
-	},
-	{
-		title: 'System',
-		role: 'not-admin',
-		sub: [
-			{
-				name: 'Settings',
-				link: '/system/settings',
-				icon: <SettingsSuggestIcon />,
-			},
-		],
-	},
+  {
+    title: 'Main',
+    role: 'not-admin',
+    sub: [
+      {
+        name: 'Dashboard',
+        link: '/main/dashboard',
+        icon: <HomeIcon />
+      },
+      {
+        name: 'Deploy',
+        link: '/main/deploy',
+        icon: <CloudIcon />
+      },
+      {
+        name: 'Image',
+        link: '/main/image',
+        icon: <StorageIcon />
+      },
+      {
+        name: 'Services',
+        link: '/main/services',
+        icon: <MarkunreadMailboxIcon />
+      },
+      {
+        name: 'Support Service',
+        link: '/main/support-service',
+        icon: <AddBoxIcon />
+      }
+    ]
+  },
+  {
+    title: 'Administrator',
+    sub: [
+      {
+        name: 'Member',
+        link: '/administrator/member',
+        icon: <PeopleAltIcon />
+      },
+      {
+        name: 'Kube Config',
+        link: '/administrator/kube-config',
+        icon: <AcUnitIcon />
+      },
+      {
+        name: 'Gitlab Config',
+        link: '/administrator/gitlab-config',
+        icon: <GitHubIcon />
+      }
+    ]
+  },
+  {
+    title: 'System',
+    role: 'not-admin',
+    sub: [
+      {
+        name: 'Settings',
+        link: '/system/settings',
+        icon: <SettingsSuggestIcon />
+      }
+    ]
+  }
 ]
